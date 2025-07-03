344efb2f3d342d229b8e2c35c6b7c8cfeabe77af
Majeed-Omer <twom764@gmail.com>
2025-07-03 16:01:21 -0700

    updated

--- main.py
+++ main.py
@@ -197,4 +197,4 @@ async def chat_history(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=" 192.168.100.106", port=8000, reload=True)
+    uvicorn.run("main:app", host="192.168.100.106", port=8000, reload=True)
