db1867081bb6b364cfef75a5b0dfa4ae1a2de97d
Majeed-Omer <twom764@gmail.com>
2025-07-07 06:01:28 -0700

    updated

--- main.py
+++ main.py
@@ -189,4 +189,4 @@ def get_cached_policies():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
